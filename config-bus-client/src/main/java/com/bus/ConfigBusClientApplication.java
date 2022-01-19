c80555820600747ca7b57b7d242615f5ced57498
lk182455145 <182455145@qq.com>
2022-01-19 21:01:19 +0800

    1

---
+++ config-bus-client/src/main/java/com/bus/ConfigBusClientApplication.java
@@ -0,0 +1,14 @@
+package com.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigBusClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigBusClientApplication.class, args);
+	}
+}
---
+++ config-bus-client/src/main/java/com/bus/controller/ConfigController.java
@@ -0,0 +1,25 @@
+package com.bus.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+	@Value("${name}")
+	private String name;
+	
+	@Value("${my.message}")
+	private String message;
+	
+	@GetMapping("name")
+	public String getname() {
+		return name;
+	}
+	
+	@GetMapping("message")
+	public String getMessage() {
+		return message;
+	}
+}
